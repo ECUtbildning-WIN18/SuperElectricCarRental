8dc3115c92a538649d940fb3c95b4017d517d324
ernestonr1 <ernstras@gmail.com>
2018-09-19 12:58:40 +0200

    Initial commit

---
+++ SuperElectricCarRental/Domain/Car.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SuperElectricCarRental.Domain {
+  class Car : Vehicle {
+    public void StartEngine() {
+      Console.WriteLine("VROOOMMM....");
+    }
+  }
+}
